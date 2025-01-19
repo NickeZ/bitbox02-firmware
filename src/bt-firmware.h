72ef805f8f2a2a21a2c50d8e2b1c2a07b18a87f1
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2025-01-19 22:46:19 +0100

    wip

---
+++ src/bt-firmware.h
@@ -0,0 +1,13 @@
+#ifndef BT_FIRMWARE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+extern const uint8_t _binary_firmware_bin_start;
+extern const uint8_t _binary_firmware_bin_end;
+extern const uint8_t _binary_firmware_bin_size;
+
+const uint8_t* bt_firmware_start = &_binary_firmware_bin_start;
+const uint8_t* bt_firmware_end = &_binary_firmware_bin_end;
+const size_t bt_firmware_size = (size_t)&_binary_firmware_bin_size;
+#endif
---
+++ src/uart.c
@@ -0,0 +1,11 @@
+#include "uart.h"
+#include "bitbox02_pins.h"
+#include "util.h"
+
+// PA04 tx -- P0_11 rx
+// PA07 rx -- P0_09 tx
+
+void uart_init(void)
+{
+    util_log("uart_init");
+}
