c0fbd3bd720817b7ee01a532a7ff23bf7af46a19
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-04-06 23:18:42 +0200

    Move Task to the only usage site

--- src/rust/util/src/bb02_async.rs
+++ src/rust/util/src/bb02_async.rs
@@ -1,13 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use alloc::boxed::Box;
-use core::pin::Pin;
-
-/// Task is the top-level future which can be polled by an executor.
-/// Note that other futures awaited inside do not have to be pinned.
-/// The 'a lifetime allows to poll a boxed/pinned future that is not
-/// 'static, or a future with non-'static input param references.
-pub type Task<'a, O> = Pin<Box<dyn core::future::Future<Output = O> + 'a>>;
-
 #[cfg(feature = "testing")]
 pub use futures_lite::future::block_on;
