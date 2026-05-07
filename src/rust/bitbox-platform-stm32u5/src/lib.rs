98e4ca85a3de07ee258a4607c5fc9d69dee237fb
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-05-07 10:36:37 +0200

    Add bitbox-platform-stm32u5

---
+++ src/rust/bitbox-platform-stm32u5/src/lib.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_std]
+
+#[cfg(all(target_arch = "arm", target_os = "none"))]
+mod inner;
+
+#[cfg(all(target_arch = "arm", target_os = "none"))]
+pub use inner::*;
